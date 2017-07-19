be75ad89457e4f43c79a266a3fd15f0853e4be92
24290 <24290@BLRDRIGWN24290.ITCINFOTECH.com>
2017-07-19 16:11:56 +0530

    comit

--- src/com/hrdocument/tests/Demo2.java
+++ src/com/hrdocument/tests/Demo2.java
@@ -1,31 +1,29 @@
 package com.hrdocument.tests;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Properties;
-
-import org.testng.annotations.Test;
 
 import generics.BaseTest;
 
 public class Demo2 extends BaseTest
 {
-	/*public static void main(String[] args) throws Exception
+		public static void main(String[] args) throws Exception
 	{
 		String path= "./config.properties";
 		Properties p = new Properties();
 		p.load(new FileInputStream(path));
 		System.out.println("url="+p.getProperty("URL"));
 		
-	}*/
-	@Test
+	}
+	
+	
+	/*@Test
 	public void testt() throws FileNotFoundException, IOException{
 		String path= "./config.properties";
 		Properties p = new Properties();
 		p.load(new FileInputStream(path));
 		System.out.println("url="+p.getProperty("URL"));
-	}
+	}*/
 	
 	
 	
